d2d9c4ef3cdfedfec2612980e0020c9864250f1d
Emanuel Tesar <e.tesarr@gmail.com>
2022-10-05 13:40:44 +0200

    Add NFT metadata

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 require('@nomicfoundation/hardhat-toolbox')
 const { getContractAt } = require('@nomiclabs/hardhat-ethers/internal/helpers')
 const { task } = require('hardhat/config')
+const fetch = require('node-fetch')
 
 const { POLYGON_MUMBAI_PROVIDER_URL, MNEMONIC, NFT_CONTRACT_ADDRESS } = process.env
 
@@ -28,6 +29,22 @@ task('mint', 'Mints from the NFT contract')
       gasLimit: 500_000,
     })
     console.log(`Transaction Hash: ${transactionResponse.hash}`)
+  })
+
+task('token-uri', 'Fetches the token metadata for the given token ID')
+  .addParam('tokenId', 'The tokenID to fetch metadata for')
+  .setAction(async function (taskArguments, hre) {
+    const contract = await getContract('NFT', hre)
+    const response = await contract.tokenURI(taskArguments.tokenId, {
+      gasLimit: 500_000,
+    })
+
+    const metadata_url = response
+    console.log(`Metadata URL: ${metadata_url}`)
+    return
+
+    const metadata = await fetch(metadata_url).then((res) => res.json())
+    console.log(`Metadata fetch response: ${JSON.stringify(metadata, null, 2)}`)
   })
 
 /** @type import('hardhat/config').HardhatUserConfig */
