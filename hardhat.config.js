6151336af3dd67f106ca3480698f153cf9803af0
Emanuel Tesar <e.tesarr@gmail.com>
2022-10-05 12:54:21 +0200

    Deploy on Polygon mumbai

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
+require('@nomicfoundation/hardhat-toolbox')
+
+const { POLYGON_MUMBAI_PROVIDER_URL, MNEMONIC } = process.env
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-};
+  solidity: '0.8.17',
+  defaultNetwork: 'polygonMumbai',
+  networks: {
+    hardhat: {},
+    polygonMumbai: {
+      url: POLYGON_MUMBAI_PROVIDER_URL,
+      accounts: { mnemonic: MNEMONIC },
+    },
+  },
+}
