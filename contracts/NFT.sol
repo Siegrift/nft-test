83be94f37735f076a344c6fe5d5f15a8452051b3
Emanuel Tesar <e.tesarr@gmail.com>
2022-10-05 13:30:35 +0200

    Use netlify lambda for NFT metadata

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,18 +5,31 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFT is ERC721 {
-    using Counters for Counters.Counter;
-    Counters.Counter private currentTokenId;
+  using Counters for Counters.Counter;
 
-    constructor() ERC721("NFTTutorial", "NFT") {}
+  Counters.Counter private currentTokenId;
 
-    function mintTo(address recipient)
-        public
-        returns (uint256)
-    {
-        currentTokenId.increment();
-        uint256 newItemId = currentTokenId.current();
-        _safeMint(recipient, newItemId);
-        return newItemId;
-    }
+  /// @dev Base token URI used as a prefix by tokenURI().
+  string public baseTokenURI;
+
+  constructor() ERC721("NFTTutorial", "NFT") {
+    baseTokenURI = "https://deploy-preview-1--airnode-mocked-api-netlify.netlify.app/metadata/";
+  }
+
+  function mintTo(address recipient) public returns (uint256) {
+    currentTokenId.increment();
+    uint256 newItemId = currentTokenId.current();
+    _safeMint(recipient, newItemId);
+    return newItemId;
+  }
+
+  /// @dev Returns an URI for a given token ID
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseTokenURI;
+  }
+
+  /// @dev Sets the base token URI prefix.
+  function setBaseTokenURI(string memory _baseTokenURI) public {
+    baseTokenURI = _baseTokenURI;
+  }
 }
